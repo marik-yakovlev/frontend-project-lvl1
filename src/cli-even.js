52ce4f8881ecb80f46d30f07e85c11b6aaac724a
Marik <marik-yakovlev@rambler.ru>
2022-03-24 18:19:07 +0300

    corrected cli-even.js

--- src/cli-even.js
+++ src/cli-even.js
@@ -1,31 +1,35 @@
 import readlineSync from 'readline-sync';
 
 const game = () => {
-  const name = readlineSync.question('May I have your name?: ');
-  console.log(`'Hello, ${name}!`);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?: ');
+  console.log(`'Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   const getRandomNumber = () => Math.floor(Math.random() * 100);
   for (let i = 1; i <= 3; i += 1) {
     const number = getRandomNumber();
     console.log(`Question: ${number}`);
-    const youAnswer = readlineSync.question('You answer: ');
+    const userAnswer = readlineSync.question('Your answer: ');
     if (number % 2 === 0) {
-      if (youAnswer === 'yes') {
+      if (userAnswer === 'yes') {
         console.log('Correct!');
       } else {
-        console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was 'yes'.`);
-        return `Let's try again, ${name}!`;
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'yes'.`);
+        console.log(`Let's try again, ${userName}!`);
+        return;
       }
     }
     if (number % 2 !== 0) {
-      if (youAnswer === 'no') {
+      if (userAnswer === 'no') {
         console.log('Correct!');
       } else {
-        console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was 'no'.`);
-        return `Let's try again, ${name}!`;
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'no'.`);
+        console.log(`Let's try again, ${userName}!`);
+        return;
       }
     }
   }
-  return `Congratulations, ${name}!`;
+  console.log(`Congratulations, ${userName}!`)
+  return;
 };
 export default game;
