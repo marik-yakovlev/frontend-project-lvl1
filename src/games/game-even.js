a2beb8331f6b4e58b87114d65ed11916776543ba
Marik <marik-yakovlev@rambler.ru>
2022-04-26 23:58:02 +0300

    refactoring game-even

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -3,12 +3,10 @@ import getRandomNumber from '../randomNumber.js';
 
 const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (number) => number % 2 === 0;
-
 const gameData = () => {
   const number = getRandomNumber(0, 100);
   const question = String(number);
-  const answer = isEven(question) ? 'yes' : 'no';
+  const answer = (number % 2 === 0) ? 'yes' : 'no';
   return [question, answer];
 };
 
