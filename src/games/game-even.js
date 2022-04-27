84d7c43f72b9c0dc9411bfc9816f06a9d0a7a0c5
Marik <marik-yakovlev@rambler.ru>
2022-04-27 21:39:33 +0300

    modified game-even

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -3,17 +3,12 @@ import getRandomNumber from '../randomNumber.js';
 
 const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
-};
+const isEven = (number) => !(number % 2);
 
 const gameData = () => {
   const number = getRandomNumber(0, 100);
   const question = String(number);
-  const answer = isEven(number);
+  const answer = isEven(question)? 'yes' : 'no';
   return [question, answer];
 };
 
