562e6f2d73960e3ae757218575270e895a5f1134
Marik <marik-yakovlev@rambler.ru>
2022-04-27 22:04:16 +0300

    fixed hexlet-check errors

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -3,12 +3,17 @@ import getRandomNumber from '../randomNumber.js';
 
 const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (num) => !(num % 2);
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
+};
 
 const gameData = () => {
   const number = getRandomNumber(0, 100);
   const question = String(number);
-  const answer = isEven(question) ? 'yes' : 'no';
+  const answer = isEven(number) ? 'yes' : 'no';
   return [question, answer];
 };
 
